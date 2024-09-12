3fe4d6f90f1536d5075c4f701b98696e9e156099
Aniket <aniketkalawat88@gmail.com>
2024-09-12 11:03:00 +0530

    bug

--- src/pages/_components/hero-section.jsx
+++ src/pages/_components/hero-section.jsx
@@ -16,16 +16,16 @@ const HeroSection = () => {
     />
     <div className="absolute inset-0 bg-black opacity-20" />
   </div>
-  <div className="relative z-10 flex flex-col justify-center items-center h-full text-center max-w-5xl mx-auto md:p-0 p-4">
-    <div data-aos="fade-up" data-aos-duration="3000" className="lg:text-[3.41rem] md:text-4xl sm:text-3xl text-xl font-bold xl:leading-tight md:mb-10 sm:mb-6">
-    We know you got a <span className="text-heading-main  drop-shadow-[0px_4px_4px_#C6A669]">fantastic Hotel,</span><span className="md:block">and that’s why you’re here!</span>
+  <div className="relative z-10 flex flex-col justify-center items-center h-full text-center max-w-5xl mx-auto md:p-0 sm:p-4 p-2">
+    <div data-aos="fade-up" data-aos-duration="3000" className="lg:text-[3.41rem] md:text-4xl sm:text-3xl text-lg font-bold xl:leading-tight md:mb-10 sm:mb-6">
+    We know you got a <span className="text-heading-main  drop-shadow-[0px_4px_4px_#C6A669]">fantastic Hotel,</span><span className="block">and that’s why you’re here!</span>
     </div>
     <p data-aos="fade-up" data-aos-duration="3000" className="md:text-2xl sm:text-xl text-sm font-medium text-[#FFFFFFCC] md:mb-8">
     Growing your hotel business has never been this easy! 
     </p>
     <div>
     <button
-        className="bg-heading-main text-primary-main hover:bg-heading-main md:py-2 md:px-6 px-1 py-1 rounded-full lg:text-lg sm:text-base text-sm font-bold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg "
+        className="bg-heading-main text-primary-main hover:bg-heading-main md:py-2 md:px-6 px-2 py-1 rounded-full lg:text-lg sm:text-base text-sm font-bold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg "
         onClick={PopupOpen}
       >
         Get Free consulation
--- src/pages/_components/why-us.jsx
+++ src/pages/_components/why-us.jsx
@@ -8,7 +8,7 @@ const WhyUs = () => {
     <div className="bg-primary-main/30 md:mt-32 md:pb-20 md:mb-20">
         <div className="max-w-7xl mx-auto lg:p-8 p-6 md:grid md:grid-cols-12 flex flex-col-reverse justify-center gap-8 relative">
             <div className="md:col-span-8">
-                <Headcomp name={'Why us'} />
+                <Headcomp name={'About us'} />
                 <div className="md:text-[1.4rem] text-xl font-semibold my-4 text-heading-main">Striving for the <span className="text-primary-main ">perfect ten!</span></div>
                 <p data-aos="fade-up" data-aos-duration="1000"  className={`text-secondary-main md:text-lg sm:text-base text-base ${poppins.className}`}>{`Our in-house team of experts is dedicated to transforming your hotel business. With a strong background in Marketing and Advertising, and experience with international clients from the USA & UAE, we're ready to deliver exactly what Indian Hotels need to dominate the market. Unlike others, we customize packages and deals tailored to your property, ensuring it reaches new heights. Our committed support team handles bookings and calls seamlessly, while our talented design team creates stunning visuals and videos to showcase your property in the best light.`}</p>
             </div>
